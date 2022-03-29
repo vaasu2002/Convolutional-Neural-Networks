8e23a9b9ce6e953e76ead64fcfbfec18403221f4
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-03-29 06:00:07 +0530

    Create Augmentation of one images.py

---
+++ Data Augmentation/Augmentation of one images.py
@@ -0,0 +1,29 @@
+from skimage import io
+from keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+        rotation_range=45,     #Random rotation between 0 and 45
+        width_shift_range=0.2,   #% shift
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='constant', cval=125)    #Also try nearest, constant, reflect, wrap
+        
+x = io.imread('/content/folder/car_0_1026.jpeg') 
+x.shape  # (200, 200, 3)
+
+# We need number of images as first dimension
+x = x.reshape((1, ) + x.shape)
+x.shape  #  (1,200, 200, 3)      
+
+
+# Saves 20 images in agumented images folder
+i = 0 
+for batch in datagen.flow(x, batch_size=16,  
+                          save_to_dir='/content/agumented images', 
+                          save_prefix='aug', 
+                          save_format='png'):
+    i += 1
+    if i > 20:
+        break 
